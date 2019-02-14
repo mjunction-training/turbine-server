49baf81ba590ee75941cc8534c6541da5f1546a3
talus005 <expogrow.org@gmail.com>
2019-02-14 22:14:48 +0530

    Initial commit

---
+++ src/main/java/com/training/mjunction/turbineserver/Application.java
@@ -0,0 +1,25 @@
+package com.training.mjunction.turbineserver;
+
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+
+@EnableAutoConfiguration
+@EnableTurbine
+@EnableDiscoveryClient
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(final SpringApplicationBuilder application) {
+		return application.sources(Application.class).web(WebApplicationType.SERVLET);
+	}
+
+	public static void main(final String[] args) {
+		new SpringApplicationBuilder(Application.class).web(WebApplicationType.SERVLET).run(args);
+	}
+}
