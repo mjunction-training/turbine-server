7d9151ad3d4359c8d0cbcfa180609aeb8d6318c7
talus005 <sanjib.x.talukdar.-nd@disney.com>
2019-05-26 14:40:16 +0530

    added bean

--- src/main/java/com/training/mjunction/turbineserver/Application.java
+++ src/main/java/com/training/mjunction/turbineserver/Application.java
@@ -7,6 +7,9 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
+import org.springframework.context.annotation.Bean;
+
+import brave.sampler.Sampler;
 
 @EnableAutoConfiguration
 @EnableTurbine
@@ -22,4 +25,9 @@ public class Application extends SpringBootServletInitializer {
 	public static void main(final String[] args) {
 		new SpringApplicationBuilder(Application.class).web(WebApplicationType.SERVLET).run(args);
 	}
+	
+	@Bean
+    public Sampler defaultSampler() {
+        return Sampler.ALWAYS_SAMPLE;
+    }
 }
